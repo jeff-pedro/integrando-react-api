ec75fe294c7fd7233774866a0c51859c89dca471
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-23 11:15:51 -0300

    adicionando paginacao de restaurantes

--- client/src/componentes/ListaRestaurantes/index.tsx
+++ client/src/componentes/ListaRestaurantes/index.tsx
@@ -1,24 +1,40 @@
-import axios from 'axios';
-import IRestaurante from '../../interfaces/IRestaurante';
-import style from './ListaRestaurantes.module.scss';
-import Restaurante from './Restaurante';
-import { useEffect, useState } from 'react';
+import axios from 'axios'
+import IRestaurante from '../../interfaces/IRestaurante'
+import { IPaginacao } from '../../interfaces/IPaginacao'
+import style from './ListaRestaurantes.module.scss'
+import Restaurante from './Restaurante'
+import { useEffect, useState } from 'react'
 
 const ListaRestaurantes = () => {
 
-  const [ restaurantes, setRestaurantes ] = useState<IRestaurante[]>([]);
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setproximaPagina] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    // obter restaurantes
+    axios
+      .get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(resposta => {
-        setRestaurantes(resposta.data.results);
+        setRestaurantes(resposta.data.results)
+        setproximaPagina(resposta.data.next)
       })
-      .catch(erro => console.log(erro));
+      .catch(erro => console.log(erro))
   }, []);
+
+  const verMais = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(resposta => {
+        setRestaurantes([...restaurantes, ...resposta.data.results])
+        setproximaPagina(resposta.data.next)
+      })
+      .catch(erro => console.log(erro))
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button onClick={verMais}> ver mais </button>}
   </section>)
 }
 
