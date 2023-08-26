3d7dfe09ccca1324393a59f144487f052e4210f8
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-25 22:53:06 -0300

    implementa açoes de próxima página e página anterior de restaurantes

--- client/src/componentes/ListaRestaurantes/index.tsx
+++ client/src/componentes/ListaRestaurantes/index.tsx
@@ -8,7 +8,8 @@ import { useEffect, useState } from 'react'
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
-  const [proximaPagina, setproximaPagina] = useState('')
+  const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
 
   useEffect(() => {
     // obter restaurantes
@@ -16,7 +17,7 @@ const ListaRestaurantes = () => {
       .get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(resposta => {
         setRestaurantes(resposta.data.results)
-        setproximaPagina(resposta.data.next)
+        setProximaPagina(resposta.data.next)
       })
       .catch(erro => console.log(erro))
   }, []);
@@ -26,7 +27,29 @@ const ListaRestaurantes = () => {
       .get<IPaginacao<IRestaurante>>(proximaPagina)
       .then(resposta => {
         setRestaurantes([...restaurantes, ...resposta.data.results])
-        setproximaPagina(resposta.data.next)
+        setProximaPagina(resposta.data.next)
+      })
+      .catch(erro => console.log(erro))
+  }
+
+  const verProximaPagina = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
+      })
+      .catch(erro => console.log(erro))
+  }
+
+  const verPaginaAnterior = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(paginaAnterior)
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
       })
       .catch(erro => console.log(erro))
   }
@@ -35,6 +58,8 @@ const ListaRestaurantes = () => {
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {proximaPagina && <button onClick={verMais}> ver mais </button>}
+    {proximaPagina && <button onClick={verProximaPagina}> próxima página </button>}
+    {paginaAnterior && <button onClick={verPaginaAnterior}> página anterior </button>}
   </section>)
 }
 
