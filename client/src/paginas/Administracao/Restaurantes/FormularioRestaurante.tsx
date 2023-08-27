a1cb624ce2517b302d8a6e43526cffc929853a3e
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-27 13:29:06 -0300

    melhora layout do formulário de cadastro de restaurantes

--- client/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ client/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material"
+import { Box, Button, TextField, Typography } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
@@ -45,15 +45,22 @@ const FormularioRestaurante = () => {
     }
 
     return (
-        <form onSubmit={aoSubmeterForm}>
-            <TextField
-                value={nomeRestaurante}
-                onChange={evento => setNomeRestaurante(evento.target.value)}
-                variant="standard"
-                label="Nome do Restaurante"
-            />
-            <Button type="submit" variant="outlined">Salvar</Button>
-        </form>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Typography component="h1" variant="h5">Formulário de Restaurantes</Typography>
+            <Box component="form" onSubmit={aoSubmeterForm}>
+                <TextField
+                    value={nomeRestaurante}
+                    onChange={evento => setNomeRestaurante(evento.target.value)}
+                    label="Nome do restaurante"
+                    variant="standard"
+                    fullWidth
+                    required
+                />
+                <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>
+                    Salvar
+                </Button>
+            </Box>
+        </Box>
     )
 }
 
