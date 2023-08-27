0b46b6f1b5c7dcce8ee65fbeaa3927813ccaaeac
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-27 12:55:25 -0300

    refatora formulário de busca de restaurantes

--- client/src/componentes/ListaRestaurantes/index.tsx
+++ client/src/componentes/ListaRestaurantes/index.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import IRestaurante from '../../interfaces/IRestaurante'
 import { IPaginacao } from '../../interfaces/IPaginacao'
 import style from './ListaRestaurantes.module.scss'
@@ -10,13 +10,49 @@ import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
+// esses são os possíveis parâmetros que podemos enviar para a API
+interface IParametrosBusca {
+  ordering?: string,
+  search?: string
+}
+
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
   const [paginaAnterior, setPaginaAnterior] = useState('')
+
   const [busca, setBusca] = useState('')
 
+  // agora, o carregarDados recebe opcionalmente as opções de configuração do axios
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+
+    axios
+      .get<IPaginacao<IRestaurante>>(url, opcoes)
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
+      })
+      .catch(erro => console.log(erro))
+  }
+
+  // a cada busca, montamos um objeto de opções
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+
+    const opcoes = {
+      params: {} as IParametrosBusca
+    }
+
+    if (busca) {
+      opcoes.params.search = busca
+    }
+
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
+  // funcionalidade de carregar paginas de dados
   // const verMais = () => {
   //   axios
   //     .get<IPaginacao<IRestaurante>>(proximaPagina)
@@ -27,28 +63,6 @@ const ListaRestaurantes = () => {
   //     .catch(erro => console.log(erro))
   // }
 
-  const carregarDados = (url: string) => {
-    axios
-      .get<IPaginacao<IRestaurante>>(url)
-      .then(resposta => {
-        setRestaurantes(resposta.data.results)
-        setProximaPagina(resposta.data.next)
-        setPaginaAnterior(resposta.data.previous)
-      })
-      .catch(erro => console.log(erro))
-  }
-
-  const buscaRestaurantes = (evento: React.FormEvent<HTMLFormElement>) => {
-    evento.preventDefault()
-
-    axios
-      .get<IPaginacao<IRestaurante>>(`http://localhost:8000/api/v1/restaurantes/?search=${busca}`)
-      .then((resposta) => {
-        setRestaurantes(resposta.data.results)
-      })
-      .catch(erro => console.log(erro))
-  }
-
   useEffect(() => {
     // obter restaurantes
     carregarDados('http://localhost:8000/api/v1/restaurantes/')
@@ -57,13 +71,20 @@ const ListaRestaurantes = () => {
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
 
-    <Paper onSubmit={buscaRestaurantes}
+    {/* <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type='submit'>buscar</button>
+    </form>
+ */}
+
+    <Paper onSubmit={buscar}
       component="form"
       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
     >
       <IconButton sx={{ p: '10px' }}> <SearchIcon /> </IconButton>
       <InputBase
         sx={{ ml: 1, flex: 1 }}
+        type="text"
         placeholder="Busca Restaurantes"
         value={busca}
         onChange={evento => setBusca(evento.target.value)}
@@ -71,8 +92,8 @@ const ListaRestaurantes = () => {
     </Paper>
 
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}> Próxima Página </button>}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}> Página Anterior </button>}
+    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}> Próxima Página </button>}
   </section>)
 }
 
