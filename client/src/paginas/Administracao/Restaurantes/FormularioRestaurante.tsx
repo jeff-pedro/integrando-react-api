77c5f5823d26aea2d2b21eb8e9c43bd80cbe40de
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-30 15:27:05 -0300

    refatora lógica do formulário de restaurantes

--- client/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ client/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,62 +1,72 @@
 import { Box, Button, TextField, Typography } from "@mui/material"
 
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 import IRestaurante from "../../../interfaces/IRestaurante"
 import http from "../../../http"
+import { Method } from "axios"
 
 const FormularioRestaurante = () => {
 
     const parametros = useParams()
-    const [nomeRestaurante, setNomeRestaurante] = useState('')
+    const navigate = useNavigate()
+
+    const [nome, setNome] = useState('')
 
     useEffect(() => {
         if (parametros.id) {
             http.admin
                 .get<IRestaurante>(`restaurantes/${parametros.id}/`)
-                .then(resposta => {
-                    setNomeRestaurante(resposta.data.nome)
-                })
-                .catch(erro => console.log(erro))
+                .then(resposta => setNome(resposta.data.nome))
         }
     }, [parametros])
 
     const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault()
 
+        let url = "restaurantes/"
+        let method: Method = "POST"
+
         if (parametros.id) {
-            http.admin
-                .put(`restaurantes/${parametros.id}/`, {
-                    nome: nomeRestaurante
-                })
-                .then(() => {
-                    alert("Restaurante atualizado com sucesso!")
-                })
-                .catch(erro => console.log(erro))
-        } else {
-            http.admin
-                .post("restaurantes/", {
-                    nome: nomeRestaurante
-                })
-                .then(() => {
-                    alert("Restaurante cadastrado com sucesso!")
-                })
-                .catch(erro => console.log(erro))
+            url += `${parametros.id}/`
+            method = "PUT"
         }
+
+        http.admin
+            .request({
+                url,
+                method,
+                data: {
+                    nome
+                }
+            })
+            .then(() => {
+                navigate("/admin/restaurantes")
+            })
     }
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
-            <Typography component="h1" variant="h5">Formulário de Restaurantes</Typography>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <Typography component="h1" variant="h5">
+                Formulário de Restaurantes
+            </Typography>
             <Box component="form" sx={{ width: "100%" }} onSubmit={aoSubmeterForm}>
                 <TextField
-                    value={nomeRestaurante}
-                    onChange={evento => setNomeRestaurante(evento.target.value)}
-                    label="Nome do restaurante"
-                    variant="standard"
-                    fullWidth
                     required
+                    value={nome}
+                    onChange={evento => setNome(evento.target.value)}
+                    margin="dense"
+                    id="nome"
+                    label="Nome"
+                    type="text"
+                    fullWidth
                 />
                 <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>
                     Salvar
