512bf4d5796e034ca6b1c2463281b746c73880a7
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-30 21:04:25 -0300

    refatora html da listagem de pratos

--- client/src/paginas/Administracao/Pratos/AdministracaoPratos.tsx
+++ client/src/paginas/Administracao/Pratos/AdministracaoPratos.tsx
@@ -1,71 +1,86 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+import { Grid, IconButton, Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import { Delete, Edit } from "@mui/icons-material";
 
 import { useEffect, useState } from "react"
+import { Link as RouterLink } from "react-router-dom";
 
 import IPrato from "../../../interfaces/IPrato"
 import http from "../../../http";
 
 const AdministracaoPratos = () => {
     // obter pratos
-    const [pratos, setPrato] = useState<IPrato[]>([])
+    const [pratos, setPratos] = useState<IPrato[]>([])
 
     useEffect(() => {
         http.admin
             .get<IPrato[]>("pratos/")
-            .then(resposta => {
-                setPrato(resposta.data)
-            })
-            .catch(erro => console.log(erro))
+            .then(resposta => setPratos(resposta.data))
     }, [])
 
-    const excluir = (pratoASerExcluido: IPrato) => {
+    const remover = (pratoARemover: IPrato) => {
         http.admin
-            .delete(`pratos/${pratoASerExcluido.id}/`)
+            .delete(`pratos/${pratoARemover.id}/`)
             .then(() => {
-                const listapratos = pratos.filter(prato => {
-                    return prato.id !== pratoASerExcluido.id
-                })
-
-                setPrato([...listapratos])
+                setPratos([
+                    ...pratos.filter(prato => prato.id !== pratoARemover.id)
+                ])
             })
-            .catch(erro => console.log(erro))
     }
 
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Nome</TableCell>
-                        <TableCell>Tag</TableCell>
-                        <TableCell>Imagem</TableCell>
-                        <TableCell>Editar</TableCell>
-                        <TableCell>Deletar</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {pratos.map(prato =>
-                        <TableRow key={prato.id}>
-                            <TableCell>{prato.nome}</TableCell>
-                            <TableCell>{prato.tag}</TableCell>
-                            <TableCell>[<a href={prato.imagem} target="_blank" rel="noreferrer">ver imagem</a>]</TableCell>
-                            <TableCell>
-                                <IconButton aria-label="editar" href={`/admin/pratos/${prato.id}`}>
-                                    <EditIcon />
-                                </IconButton>
-                            </TableCell>
-                            <TableCell>
-                                <IconButton aria-label="deletar" onClick={() => excluir(prato)}>
-                                    <DeleteIcon />
-                                </IconButton>
-                            </TableCell>
+        <>
+            <Grid container>
+                <Grid item xs>
+                    <Typography component="h1" variant="h6">
+                        Pratos
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Link
+                        variant="button"
+                        component={RouterLink}
+                        to={"/admin/pratos/novo/"}
+                    >
+                        Novo
+                    </Link>
+                </Grid>
+            </Grid>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Nome</TableCell>
+                            <TableCell>Tag</TableCell>
+                            <TableCell>Imagem</TableCell>
+                            <TableCell colSpan={2} align="center">Ações</TableCell>
                         </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {pratos.map(prato =>
+                            <TableRow key={prato.id}>
+                                <TableCell>{prato.nome}</TableCell>
+                                <TableCell>{prato.tag}</TableCell>
+                                <TableCell>[<a href={prato.imagem} target="_blank" rel="noreferrer">ver imagem</a>]</TableCell>
+                                <TableCell align="center">
+                                    <Link
+                                        variant="button"
+                                        component={RouterLink}
+                                        to={`/admin/pratos/${prato.id}`}
+                                    >
+                                        <IconButton sx={{ marginRight: 3 }} aria-label="editar">
+                                            <Edit />
+                                        </IconButton>
+                                    </Link>
+                                    <IconButton aria-label="deletar" onClick={() => remover(prato)}>
+                                        <Delete />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     )
 }
 
